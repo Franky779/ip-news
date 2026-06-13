f3e04ef2763d6917eaf13143c44c827ac4e891f3
Franky779 <141043756+Franky779@users.noreply.github.com>
2026-06-14 00:00:18 +0800

    更新报告列表 (2026-06-13)

--- articles-data.js
+++ articles-data.js
@@ -1,52 +1,42 @@
 const ARTICLES = [
     {
-        title: "别碰二次元，碰就是死！",
-        date: "2026-05-07",
-        url: "https://mp.weixin.qq.com/s/YCv7oSVyU94J8W8agSHb8A"
+        "title": "【深度】30天，4万+字，我手搓了一份卡牌赛道深度报告，让我发现了7个行业真相和6个预判",
+        "url": "https://mp.weixin.qq.com/s/pt6ZZS62KScygA3Wf2dFvg",
+        "date": "2026-06-05"
     },
     {
-        title: "满大街都是\"类似Labubu\"和\"类似娃三岁\"，你们到底在恶心谁?",
-        date: "2026-05-06",
-        url: "https://mp.weixin.qq.com/s/lr1kbxn6ldMSKYg7gwZ1AA"
+        "title": "Q1烧掉20亿，瑞幸KFC们的联名还卖得动吗？",
+        "url": "https://mp.weixin.qq.com/s/BmG6xWl54BpZRiDXZJ4UAA",
+        "date": "2026-05-18"
     },
     {
-        title: "笑死！那个拿下哪吒1亿订单的工厂，现在要倒闭了....真相远比你想的残酷",
-        date: "2026-04-19",
-        url: "https://mp.weixin.qq.com/s/qaS1yz2OTUxx0JgaUGyRsA"
+        "title": "当人人都能一键AI出图，IP行业最大的谎言被戳穿了",
+        "url": "https://mp.weixin.qq.com/s/V7SiPgaqkf8BpFm3YVstCw",
+        "date": "2026-05-15"
     },
     {
-        title: "【盘点】五一期间上新的IP授权案例",
-        date: "2026-05-01",
-        url: "https://mp.weixin.qq.com/s/xvVBZ8MS97gxRYeHmFm9Fw"
+        "title": "别碰二次元，碰就是死！",
+        "url": "https://mp.weixin.qq.com/s/YCv7oSVyU94J8W8agSHb8A",
+        "date": "2026-05-07"
     },
     {
-        title: "【深度盘点分析】哆啦A梦2026年IP授权联名案例。这世上根本就没有\"新IP红利\"！",
-        date: "2026-04-28",
-        url: "https://mp.weixin.qq.com/s/4ldB2hyuWNgx2WAp6RiI6g"
+        "title": "满大街都是\"类似Labubu\"和\"类似娃三岁\"，你们到底在恶心谁？",
+        "url": "https://mp.weixin.qq.com/s/lr1kbxn6ldMSKYg7gwZ1AA",
+        "date": "2026-05-06"
     },
     {
-        title: "周深做了IP，一年卖了1个亿：这不是好消息",
-        date: "2026-04-27",
-        url: "https://mp.weixin.qq.com/s/4y6US7lK5y9ArHdJA0pPnQ"
+        "title": "【盘点】五一期间上新的IP授权案例",
+        "url": "https://mp.weixin.qq.com/s/xvVBZ8MS97gxRYeHmFm9Fw",
+        "date": "2026-05-01"
     },
     {
-        title: "凌晨2点，那个做了7年IP授权的朋友说要去卖保险了..",
-        date: "2026-04-24",
-        url: "https://mp.weixin.qq.com/s/z8lYek77Mzh8b1d8REnR3A"
+        "title": "【深度盘点分析】哆啦A梦2026年IP授权联名案例。这世上根本就没有\"新IP红利\"！",
+        "url": "https://mp.weixin.qq.com/s/4ldB2hyuWNgx2WAp6RiI6g",
+        "date": "2026-04-28"
     },
     {
-        title: "我算了一笔账，然后放弃了AI漫剧——这个行业正在活埋老实人",
-        date: "2026-04-19",
-        url: "https://mp.weixin.qq.com/s/LICmypOt99NgZErAtN6vFA"
-    },
-    {
-        title: "【避雷】谁再让你做IP快闪，直接干死TA.......",
-        date: "2026-04-14",
-        url: "https://mp.weixin.qq.com/s/nfgvkwdkO8ciotkdkqB91A"
-    },
-    {
-        title: "IP\\潮玩行业动态汇总【2026.3.13-4.13】",
-        date: "2026-04-13",
-        url: "https://mp.weixin.qq.com/s/FiZShHyXiGgKst-JKh3geQ"
+        "title": "笑死！那个拿下哪吒1亿订单的工厂，现在要倒闭了....真相远比你想的残酷",
+        "url": "https://mp.weixin.qq.com/s/qaS1yz2OTUxx0JgaUGyRsA",
+        "date": "2026-04-19"
     }
 ];
